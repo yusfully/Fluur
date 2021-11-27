806f3cee21a11ccdc2757103dcd048452a709eaf
yusufozturk-jotform <yusuf06ozturk@gmail.com>
2021-11-27 10:16:27 +0300

    Clean::clean debuggers

--- src/Components/SlideUpMenu/index.js
+++ src/Components/SlideUpMenu/index.js
@@ -11,63 +11,59 @@ import Svg from "../Svg";
 import { gsap } from "gsap";
 import "./slideUp.css";
 
-const SlideUpMenu = forwardRef(
-  ({ selected, items, onSelect, onOpen, initial }, ref) => {
-    const slideRef = useRef();
-    const buttonRef = useRef();
-    const [isOpened, setIsOpened] = useState(false);
-    debugger;
-    useImperativeHandle(ref, () => ({
-      close() {
-        setIsOpened(false);
-      },
-    }));
-    const handleSelect = useCallback((item) => {
-      debugger;
-      onSelect && onSelect(item);
+const SlideUpMenu = forwardRef(({ selected, items, onSelect, onOpen }, ref) => {
+  const slideRef = useRef();
+  const buttonRef = useRef();
+  const [isOpened, setIsOpened] = useState(false);
+  useImperativeHandle(ref, () => ({
+    close() {
       setIsOpened(false);
-    }, []);
+    },
+  }));
+  const handleSelect = useCallback((item) => {
+    onSelect && onSelect(item);
+    setIsOpened(false);
+  }, []);
 
-    useEffect(() => {}, [selected]);
+  useEffect(() => {}, [selected]);
 
-    useEffect(() => {
-      isOpened && onOpen && onOpen();
-    }, [isOpened]);
+  useEffect(() => {
+    isOpened && onOpen && onOpen();
+  }, [isOpened]);
 
-    useEffect(() => {
-      gsap.to(slideRef.current, 0.5, {
-        height: `${isOpened ? "auto" : "0"}`,
-        ease: "power4.out",
-      });
-    }, [isOpened]);
-    return (
-      <div className={`${isOpened ? "isOpened" : ""} slideUp-wrapper`}>
-        <div ref={slideRef} className="slideUp">
-          {items &&
-            items.map((item) => {
-              return (
-                <button
-                  className={`${
-                    selected === item ? "selected" : ""
-                  } slideUp-button`}
-                  type="button"
-                  onClick={() => handleSelect(item)}
-                >
-                  <Svg name={`${item}`} />
-                </button>
-              );
-            })}
-        </div>
-        <div
-          ref={buttonRef}
-          onClick={() => setIsOpened(!isOpened)}
-          className="slideUp-selected"
-        >
-          <Svg name={`${!isOpened ? selected : "close"}`} />
-        </div>
+  useEffect(() => {
+    gsap.to(slideRef.current, 0.5, {
+      height: `${isOpened ? "auto" : "0"}`,
+      ease: "power4.out",
+    });
+  }, [isOpened]);
+  return (
+    <div className={`${isOpened ? "isOpened" : ""} slideUp-wrapper`}>
+      <div ref={slideRef} className="slideUp">
+        {items &&
+          items.map((item) => {
+            return (
+              <button
+                className={`${
+                  selected === item ? "selected" : ""
+                } slideUp-button`}
+                type="button"
+                onClick={() => handleSelect(item)}
+              >
+                <Svg name={`${item}`} />
+              </button>
+            );
+          })}
       </div>
-    );
-  }
-);
+      <div
+        ref={buttonRef}
+        onClick={() => setIsOpened(!isOpened)}
+        className="slideUp-selected"
+      >
+        <Svg name={`${!isOpened ? selected : "close"}`} />
+      </div>
+    </div>
+  );
+});
 
 export default SlideUpMenu;
